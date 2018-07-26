a0247f011f94c95a53740000462e3e7e90556a3b
Dybing <sdybing@igskgacggsaslrs.cr.usgs.gov>
2018-07-26 15:59:07 -0600

    test

--- Figure2.py
+++ Figure2.py
@@ -8,6 +8,8 @@ import numpy as np
 from obspy.signal.invsim import evalresp
 debug = True
 
+# Can I work github?
+
 # Import/apply font
 mpl.rc('font',family='serif')
 mpl.rc('font',serif='Times') 
